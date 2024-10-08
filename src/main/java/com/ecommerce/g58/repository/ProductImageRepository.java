1369935e94284b78bf4c4b8f3ecfd8a61458e208
Nguyễn Đình Khôi <khoindhe170734@fpt.edu.vn>
2024-10-08 21:11:06 +0700

    fix search bar no image

--- src/main/java/com/ecommerce/g58/repository/ProductImageRepository.java
+++ src/main/java/com/ecommerce/g58/repository/ProductImageRepository.java
@@ -3,10 +3,15 @@ package com.ecommerce.g58.repository;
 import com.ecommerce.g58.entity.ProductImage;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 @Repository
 public interface ProductImageRepository extends JpaRepository<ProductImage, Integer> {
-    List<ProductImage> findByProductProductId(Integer productId); // Fetch images by productId
+
+    // Query to fetch images by joining product_variation and product_image using image_id
+    @Query("SELECT pi FROM ProductImage pi JOIN ProductVariation pv ON pi.imageId = pv.productImage.imageId WHERE pv.productId.productId = :productId")
+    List<ProductImage> findByProductProductId(@Param("productId") Integer productId);
 }
