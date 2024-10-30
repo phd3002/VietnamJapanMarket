d007936b048799e92ab10b959dcb564b3ecf0197
Trinh Tu Bao <baotthe163571@fpt.edu.vn>
2024-10-30 19:02:11 +0700

    Update order rating

--- src/main/java/com/ecommerce/g58/service/OrderDetailService.java
+++ src/main/java/com/ecommerce/g58/service/OrderDetailService.java
@@ -7,5 +7,7 @@ public interface OrderDetailService {
 
     // Method to fetch order details by orderId
     List<OrderDetailDTO> getOrderDetails(Long orderId);
+
+    void rateOrder(Long orderId, String userEmail, String rateText, Integer rateStar);
 }
 
