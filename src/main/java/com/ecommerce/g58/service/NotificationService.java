82241dbcff5aade4f22308f7a280befae224911e
ducanhtran962 <anhtdhe161165@fpt.edu.vn>
2024-11-25 19:36:54 +0700

    notification

--- src/main/java/com/ecommerce/g58/service/NotificationService.java
+++ src/main/java/com/ecommerce/g58/service/NotificationService.java
@@ -13,5 +13,6 @@ public interface NotificationService {
     Integer getUnreadNotificationCount();
 
     String markAsRead(Long notificationId);
+
     void updateNotification(Notification notification) ;
 }
