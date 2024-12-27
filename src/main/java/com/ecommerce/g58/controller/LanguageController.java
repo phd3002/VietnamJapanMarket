765c25fc9f15fb42950194274f789e52b264dd72
ducanhtran962 <anhtdhe161165@fpt.edu.vn>
2024-12-27 21:48:59 +0700

    term of service

--- src/main/java/com/ecommerce/g58/controller/LanguageController.java
+++ src/main/java/com/ecommerce/g58/controller/LanguageController.java
@@ -4,16 +4,23 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 @Controller
 public class LanguageController {
 
     @GetMapping("/change-language")
-    public String setLanguage(@RequestParam(value = "lang") String lang, HttpSession session) {
+    public String setLanguage(@RequestParam(value = "lang") String lang,
+                              HttpServletRequest request, HttpSession session) {
         if (lang != null) {
             session.setAttribute("lang", lang);
         }
-        return "redirect:/";
+
+        // Lấy URL trước đó (referer)
+        String referer = request.getHeader("Referer");
+
+        // Trả về redirect tới URL trước đó
+        return "redirect:" + (referer != null ? referer : "/");
     }
 }
